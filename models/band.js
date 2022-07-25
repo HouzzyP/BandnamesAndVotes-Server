8362b18d428d4ea7f8905f7e79ce6e0bf53d42a6
HouzzyP <choclo99@gmail.com>
2022-07-25 01:11:50 -0300

    First and unique commit, server-side of bandnames proyect

---
+++ models/band.js
@@ -0,0 +1,20 @@
+const {v4: uuidv4} = require('uuid')
+
+
+class Band {
+
+
+	constructor(name){
+
+		this.id = uuidv4()
+		this.name = name;
+		this.votes = 0;
+	}
+
+
+	
+
+
+}
+
+module.exports = Band
